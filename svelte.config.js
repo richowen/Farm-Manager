bad31a1f28d69adabe9dbe821defc00f8b52c21a
richowen <richowen11@hotmail.com>
2026-05-18 17:28:51 +0100

    fix: move bodySizeLimit into adapter() call (svelte.config.js)

--- svelte.config.js
+++ svelte.config.js
@@ -5,10 +5,7 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 const config = {
   preprocess: vitePreprocess(),
   kit: {
-    adapter: adapter({ out: 'build' }),
-    server: {
-      bodySizeLimit: 1024 * 1024 * 1024 // 1 GB
-    }
+    adapter: adapter({ out: 'build', bodySizeLimit: 1024 * 1024 * 1024 }) // 1 GB
   },
   compilerOptions: {}
 };
